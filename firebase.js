36da0afd162b4ecd7f500daac4ee3e30971f09ab
Aqeel Shahzad <67902032+aqeel-spec@users.noreply.github.com>
2023-02-25 16:41:46 +0500

    firebase and nextauth  athentication added in api route

---
+++ firebase.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp, getApp, getApps } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: "twitter-clone-app-eaee9.firebaseapp.com",
+    projectId: "twitter-clone-app-eaee9",
+    storageBucket: "twitter-clone-app-eaee9.appspot.com",
+    messagingSenderId: "597313025214",
+    appId: "1:597313025214:web:b9f337672a6b9a7d9a9aef"
+};
+
+// Initialize Firebase
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+const db = getFirestore();
+const storage = getStorage();
+
+export { app, db, storage }
