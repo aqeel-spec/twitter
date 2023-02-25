0a8fd7f5178a9e9d7d9ee2b5d2b069ea7e1a9e60
Aqeel Shahzad <67902032+aqeel-spec@users.noreply.github.com>
2023-02-26 00:07:51 +0500

    firebase athentication added with some changes had to make later such as nextAuthUrl

--- firebase.js
+++ firebase.js
@@ -7,7 +7,7 @@ import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: process.env.FIREBASE_API_KEY,
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
     authDomain: "twitter-clone-app-eaee9.firebaseapp.com",
     projectId: "twitter-clone-app-eaee9",
     storageBucket: "twitter-clone-app-eaee9.appspot.com",
--- pages/api/auth/[...nextauth].tsx
+++ pages/api/auth/[...nextauth].tsx
@@ -1,15 +1,29 @@
 import NextAuth, { NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import { AuthOptions } from "next-auth";
+import { GoogleProfile } from "next-auth/providers/google";
+import { OAuthConfig } from "next-auth/providers";
 
-export const authOptions: NextAuthOptions = {
+// GOOLE_CLIENT_ID = 597313025214-9a4tbjhp4sj9oia1pkvrr638qir9ndvf.apps.googleusercontent.com;
+// GOOGLE_CLEINT_SECRET = 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;
+
+type ClientID = {
+  cleintId: string;
+  clientSecret: string;
+};
+export const authOptions: AuthOptions = {
   // Configure one or more authentication providers
   providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    GoogleProvider<GoogleProfile>({
+      clientId:
+        "597313025214-9a4tbjhp4sj9oia1pkvrr638qir9ndvf.apps.googleusercontent.com",
+      clientSecret: "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",
     }),
     // ...add more providers here
   ],
+  pages: {
+    signIn: "/auth/signin",
+  },
 };
 
 export default NextAuth(authOptions);
